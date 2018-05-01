22b3c42f5f141d4486f5f4a5215c7fed1b2ab9ba
Patrice <patweb44@gmail.com>
2018-05-01 11:51:18 +0000

    Added build processes by webpack

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,40 @@
+const config = require('./webpack.base')
+const webpack = require('webpack')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const extractCSS = new ExtractTextPlugin('bundle.css');
+
+config.plugins = config.plugins.concat([
+  extractCSS,
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+      screw_ie8: true,
+      conditionals: true,
+      unused: true,
+      comparisons: true,
+      sequences: true,
+      dead_code: true,
+      evaluate: true,
+      if_return: true,
+      join_vars: true
+    },
+    output: {
+      comments: false
+    }
+  }),
+  
+   new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  })
+])
+
+// Css-loader - get CSS and style
+// Can add in the futur some pre-processed CSS
+let cssLoaders = config.module.loaders[0].loaders
+config.module.loaders[0].loaders = null
+// Get loader CSS and remove first value
+config.module.loaders[0].loader = extractCSS.extract(cssLoaders.slice(1, 10))
+
+module.exports = config
